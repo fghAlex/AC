e980e8b087177d29ae1f0e3d1308451f00e8a85e
Alex <fghuggv47@gmail.com>
2025-05-11 15:29:06 +0300

    init

---
+++ __main__.py
@@ -0,0 +1,85 @@
+import os
+import subprocess
+import time
+import start_noir
+
+def check_start_containers():
+    time.sleep(5)
+
+    # Получаем статус контейнеров
+    output = subprocess.check_output(['docker-compose', 'ps'])
+    lines = output.decode().splitlines()
+
+    # Проверяем, есть ли контейнеры в выводе (минимум 3 строки: заголовок, разделитель и данные)
+    if len(lines) < 3:
+        print("Ошибка: контейнеры не найдены.")
+        while i < 100:
+            time.sleep(10)
+
+            output = subprocess.check_output(['docker-compose', 'ps'])
+            lines = output.decode().splitlines()
+            i=i+1
+            print(f"Ожидание запуска контейнеров. Выполнено попыток проверки: {i}")
+
+            if len(lines) > 3:
+                return True
+    return True
+
+def main():
+    # Получаем текущую директорию, где запущен скрипт
+    current_dir = os.getcwd()
+    
+    # Получаем и сортируем список директорий в текущей папке
+    directories = sorted([d for d in os.listdir() if os.path.isdir(d)])
+
+    if not directories:
+        print("В текущей папке нет директорий.")
+        exit()
+
+    # список директорий с номерами
+    for i, dir in enumerate(directories, 1):
+        print(f"{i}. {dir}")
+
+    choice = int(input("Введите номер выбранного проекта: "))
+    selected_proj = directories[choice - 1]
+    flow_proj = current_dir + "/" + selected_proj
+    subprocess.run("cd", cwd=flow_proj)
+
+    ######################
+    ###CONTAINRES START###
+    ######################
+
+    current_dir = os.getcwd()
+    compose_file = os.path.join(current_dir, 'docker-compose.yml')
+
+    # Проверяем, существует ли файл docker-compose.yml
+    if not os.path.isfile(compose_file):
+        print("docker-compose.yml не найден в текущей директории.")
+        exit(1)
+        compose_file = os.path.join(current_dir, 'compose.yml')
+        if not os.path.isfile(compose_file):
+            print("compose.yml не найден в текущей директории.")
+            exit(1)
+    
+    # Запускаем docker-compose up -d
+    print("Запуск контейнеров")
+    try:
+        subprocess.run(['docker-compose', 'up', '-d'], check=True)
+        print("docker-compose up -d выполнено.")
+    except FileNotFoundError:
+        print("Ошибка: docker-compose не найден. Убедитесь, что docker-compose установлен.")
+        exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при выполнении docker-compose up -d: {e}")
+        exit(1)
+
+    check_start_containers()
+
+    ################
+    ###NOIR START###
+    ################
+    start_noir.check_dir()
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ start_noir.py
@@ -0,0 +1,101 @@
+import os
+import subprocess
+
+### на случай если захочется редактировать содержимое трафика.
+### его удобнее редактировать в формате списка curl запросов 
+# def send_traffic():
+#     # Формируем путь к файлу endpoints.flow в выбранной директории
+#     flow_file = os.path.join(selected_dir, "endpoints.flow")
+    
+#     # Проверяем, существует ли файл endpoints.flow
+#     if not os.path.isfile(flow_file):
+#         print(f"Ошибка: файл {flow_file} не найден в директории {selected_dir}.")
+#         return
+    
+#     # Формируем команду для mitmdump
+#     command = [
+#         "mitmdump",
+#         "-r", flow_file,
+#         "--set", "stream_large_bodies=0",
+#         "--export-curl"
+#     ]
+    
+#     # Путь к файлу requests.sh в выбранной директории
+#     output_file = os.path.join(selected_dir, "requests.sh")
+    
+#     try:
+#         # Выполняем команду и перенаправляем вывод в requests.sh
+#         with open(output_file, "w") as f:
+#             subprocess.run(command, stdout=f, check=True)
+#         print(f"Команда mitmdump успешно выполнена, результат сохранен в {output_file}.")
+#     except FileNotFoundError:
+#         print("Ошибка: mitmdump не найден. Убедитесь, что mitmproxy установлен.")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Ошибка при выполнении mitmdump: {e}")
+
+
+def start_noir(noir_path: str, target_dir: str):
+    target_domain_name = input("Укажите адрес и порт Объекта Оценки): ")
+
+    if not os.path.isfile(noir_path):
+        raise FileNotFoundError(f"Noir не найден по пути: {noir_path}")
+    
+    if not os.path.isdir(target_dir):
+        raise NotADirectoryError(f"Указанный каталог не существует: {target_dir}")
+    
+    try:
+        result = subprocess.run([noir_path, " -b ", target_dir, " -u ", 
+                                target_domain_name, " --send-proxy http://localhost:8081 ", 
+                                "--format", " json", " --output ", "noir_output.json"], check=False)
+        return result.returncode
+    except Exception as e:
+        raise RuntimeError(f"Ошибка при запуске Noir: {e}")
+
+
+def start_proxy(selected_dir):
+    # TO DO:
+    output_file = ""
+    # Формируем команду для mitmproxy
+    command = [
+        "mitmproxy",
+        "-w", "traffic.flow",
+        "-p", "8081",
+        "--set", "stream_large_bodies=0"
+    ]
+    # TO DO:
+    print(f"Трафик записан в файл: {output_file}")
+
+
+def check_dir():
+    # Получаем и сортируем список директорий в текущей папке
+    directories = sorted([d for d in os.listdir() if os.path.isdir(d)])
+
+    if not directories:
+        print("В текущей папке нет директорий.")
+        exit()
+
+    #список директорий с номерами
+    for i, dir in enumerate(directories, 1):
+        print(f"{i}. {dir}")
+
+    try:
+        choice = int(input("Введите номер директории с исходным кодом ОО: "))
+        selected_dir = directories[choice - 1]
+        
+        ########################
+        ###START PROXY & NOIR###
+        ########################
+        start_proxy(selected_dir)
+        exit_code = start_noir("/opt/noir/bin/noir", selected_dir)
+        if exit_code == 0:
+            print("Анализ завершён успешно.")
+        else:
+            print(f"NOIR ERROR: {exit_code}")
+
+    except IndexError:
+        print("Неправильный номер директории.")
+
+
+#
+# --set stream_large_bodies=0 флаг для оптимизации работы прокси
+#        subprocess.run(["mitmdump -r endpoints.flow --export-curl > requests.sh", selected_dir])
