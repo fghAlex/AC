19d191b909b44b522083c72032b9318369792113
Alex <fghuggv47@gmail.com>
2025-05-14 00:15:08 +0300

    R4c

--- natch_start.py
+++ natch_start.py
@@ -1,7 +1,7 @@
 import os
+import configparser
 
-def check_dir():
-    current_directory = os.getcwd()
+def check_dir(current_directory):
 
     # Расширение файла для проверки
     file_extension = '.qcow2'
@@ -18,8 +18,49 @@ def check_dir():
         print(f"В текущей директории {current_directory} не найден образ ВМ.")
         exit(1)
 
+def natch_create_config():
+
+    # Создаем объект ConfigParser
+    config = configparser.ConfigParser()
+
+    # Добавляем секцию Settings
+    config['Settings'] = {
+        'version': '8',
+        'arch': 'x86_64',
+        'mount': 'True',
+        'ram': '4G',
+        'emu_mode': 'graphic',
+        'port_forwarding': 'True',
+        'source_ports': 'True',
+        'module_config': 'True',
+        'tuning': 'True',
+        'debug_info': 'True'
+    }
+
+    # Запрашиваем ввод пользователя для двух переменных
+    ports_str_value = input("Введите значение для пометки и проброса портов: ")
+    guest_module_dir_value = input("Введите путь для бинарными файлами в гостевой ОС: ")
+
+    # Устанавливаем введенные значения
+    config.set('Settings', 'ports_str', ports_str_value)
+    config.set('Settings', 'guest_module_dir', guest_module_dir_value)
+
+    # Сохраняем изменения в файле settings_test.ini
+    with open('settings_test.ini', 'w') as configfile:
+        config.write(configfile)
+
+    print(f'Файл settings_test.ini успешно создан.')
+    return
+
+def natch_create_proj():
+    return
+
 def start():
     
-    check_dir()
-    
+    current_directory = os.getcwd()
+    check_dir(current_directory)
+    print("Начинается создание проекта natch.")
+    natch_create_config()
+    natch_create_proj()
+
     return
